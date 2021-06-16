96616b75223149b5ed268cda57ae10b52e1af154
Isaac Miti <ikayz@live.com>
2021-06-17 00:18:49 +0200

    Add navigation structure and core files for the different pages

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,39 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Journals from "./Journal";
+import LandingPage from "./Landing";
+import NewsArticles from "./NewsArticles";
+import Contact from "./Contact";
+import About from "./About";
+import Manifesto from "./Manifesto";
 
-function home() {
-  return <Router></Router>;
-}
+const home = () => {
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <LandingPage />
+          </Route>
+          <Route path="/journals">
+            <Journals />
+          </Route>
+          <Route path="/newsarticles">
+            <NewsArticles />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/Manifesto">
+            <Manifesto />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+};
+
+export default home;
