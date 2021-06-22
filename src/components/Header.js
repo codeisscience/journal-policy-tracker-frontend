ad45da1ee886997d8b073e112809c9ff29b92ac8
Isaac Miti <ikayz@live.com>
2021-06-22 19:09:18 +0200

    Add header image and section

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+
+function Header() {
+  return (
+    <div className="header">
+      <Container fluid>
+          Header
+      </Container>
+    </div>
+  )
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navigation from './components/Navigation';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
       <Navigation />
+      <Header />
     </>
   );
 }
