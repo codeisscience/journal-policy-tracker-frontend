0bc44fe345e1321834e0dc43170cd4e8ec395ff0
Isaac Miti <ikayz@live.com>
2021-06-23 22:48:35 +0200

    Remove wrong reference asset

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+function Footer() {
+  return (
+    <Container>
+      <Row>
+        <Col xs={{ order: "last" }}>First, but last</Col>
+        <Col xs>Second, but unordered</Col>
+        <Col xs={{ order: "first" }}>Third, but first</Col>
+      </Row>
+      <Row className="footer-below">
+        <p>2021 Code is Science</p>
+      </Row>
+    </Container>
+  );
+}
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navigation from './components/Navigation';
-import Header from './components/Header';
+import Navigation from "./components/Navigation";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Navigation />
       <Header />
+      <Footer />
     </>
   );
 }
