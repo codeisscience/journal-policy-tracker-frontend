0f38191433994f041271b023e37c629255b89e19
Isaac Miti <ikayz@live.com>
2021-06-24 23:38:46 +0200

    Add premise section

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -10,7 +10,21 @@ function LandingSection() {
           <Col md={6}>
             <img src={Img} width={500} responsive />
           </Col>
-          <Col md={6}>Some text</Col>
+          <Col md={6}>
+            <h1>Premise</h1>
+            <h2>Science is often computing</h2>
+            <p>
+              Much of modern science involves code these days, in large part due
+              to the amount of data available - it would be almost impossible to
+              analyse without computational assistance.
+            </p>
+            <h2>Science requires peer review</h2>
+            <p>
+              One of the basic prerequisites for any published scientific
+              results is that it be reviewed by peers, to ensure the research
+              and conclusions are valid.
+            </p>
+          </Col>
         </Row>
       </Container>
     </section>
