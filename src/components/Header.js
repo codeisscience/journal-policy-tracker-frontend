dbc12fa240aa8050f7db12dc0985b24ed98300d2
Isaac Miti <ikayz@live.com>
2021-06-25 00:32:16 +0200

    Add button to header and styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
+import React from "react";
+import { Button } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
 
 function Header() {
   return (
     <div className="header">
       <Container fluid>
-          Header
+        <button>Read and Sign the Manifesto</button>
       </Container>
     </div>
-  )
+  );
 }
 
 export default Header;
