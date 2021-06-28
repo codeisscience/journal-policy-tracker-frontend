5cb0920bcefb3d96f895a2cba4665c471d397345
Isaac Miti <ikayz@live.com>
2021-06-28 21:23:39 +0200

    Destruct bootstrap components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,5 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import { Container, Row, Col } from "react-bootstrap";
 import RocketImg from "./assets/rocket.png";
 
 function Footer() {
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,14 @@
 import React from "react";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import Logo from './assets/logo.png';
+import { Navbar, Nav } from "react-bootstrap";
+import Logo from "./assets/logo.png";
 
 function Navigation() {
   return (
     <>
       <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-        <Navbar.Brand href="#home"><img src={Logo} alt="" srcset="" width={150}/></Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <img src={Logo} alt="" srcset="" width={150} />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav>
@@ -15,10 +16,10 @@ function Navigation() {
             <Nav.Link eventKey={2} href="#journals">
               Journals
             </Nav.Link>
-            <Nav.Link  href="#manifesto">Manifesto</Nav.Link>
-            <Nav.Link  href="#blog">Blog</Nav.Link>
-            <Nav.Link  href="#blog">Contact</Nav.Link>
-            <Nav.Link  href="#blog">About</Nav.Link>
+            <Nav.Link href="#manifesto">Manifesto</Nav.Link>
+            <Nav.Link href="#blog">Blog</Nav.Link>
+            <Nav.Link href="#blog">Contact</Nav.Link>
+            <Nav.Link href="#blog">About</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
