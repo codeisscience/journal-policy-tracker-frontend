1f3b6953ac287010cb79288346b4a83f6ff9866f
Isaac Miti <ikayz@live.com>
2021-06-30 23:08:30 +0200

    Finish manifesto section one

--- src/components/Manifesto.js
+++ src/components/Manifesto.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import StandingImg from "./assets/human-2.png";
 import SittingImg from "./assets/human-1.png";
+import ImgOne from "./assets/manifesto-img-1.png";
 
 const Manifesto = () => {
   return (
@@ -15,6 +16,22 @@ const Manifesto = () => {
         </Col>
         <Col md={3}>
           <img src={SittingImg} height={220} />
+        </Col>
+      </Row>
+      <Row className="manifesto-section-one">
+        <Col md={6}>
+          <img src={ImgOne} width="500" />
+        </Col>
+        <Col md={6}>
+          <h1>Code is Science Manifesto</h1>
+          <p>
+            Through working with scientific code, we agree that scientific code
+            needs to be treated as a genuine research output. For this reason we
+            have created a manifesto, which will allow both individuals and
+            organisations to sign up and agree to make genuine efforts to both
+            make their own scientific code open, as well as sharing the
+            manifesto mission with others.
+          </p>
         </Col>
       </Row>
     </Container>
