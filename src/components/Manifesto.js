2e568ae8ab2a40df9efeea105310f4adb5999dc6
Isaac Miti <ikayz@live.com>
2021-06-30 22:50:38 +0200

    Add images on the left and right

--- src/components/Manifesto.js
+++ src/components/Manifesto.js
@@ -1,18 +1,20 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import StandingImg from "./assets/human-2.png";
+import SittingImg from "./assets/human-1.png";
 
 const Manifesto = () => {
   return (
     <Container>
-      <Row>
+      <Row className="manifesto-header">
         <Col md={3}>
-          <img src />
+          <img src={StandingImg} height={220} />
         </Col>
         <Col md={6} className="manifesto">
           <h1>Manifesto</h1>
         </Col>
         <Col md={3}>
-          <img src />
+          <img src={SittingImg} height={220} />
         </Col>
       </Row>
     </Container>
