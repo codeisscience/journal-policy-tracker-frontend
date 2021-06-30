331037b78420de5fbbec395712483023e4ceccc5
Isaac Miti <ikayz@live.com>
2021-06-30 22:21:45 +0200

    Add manifesto starter code

--- src/components/Manifesto.js
+++ src/components/Manifesto.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Manifesto = () => {
-  return <div>Manifesto</div>;
+  return (
+    <Container>
+      <Row>
+        <Col md={3}>
+          <img src />
+        </Col>
+        <Col md={6}>
+          <h1>Manifesto</h1>
+        </Col>
+        <Col md={3}>
+          <img src />
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Manifesto;
