37bc9b7c20ad87499bccd6a3c4be4d96a14b6cfa
CIPHERTron <pritish.samal918@gmail.com>
2021-08-13 00:34:25 +0530

    fix: CORS error & fetch token

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,89 +1,153 @@
-import React from "react";
-import { Col, Row, Form, Button } from "react-bootstrap";
+import React from "react"
+import { Col, Row, Form, Button } from "react-bootstrap"
 
 class Login extends React.Component {
-  state = {
-    email: "",
-    password: "",
-  };
+	state = {
+		email: "",
+		password: "",
+		accessToken: "",
+	}
 
-  handleLogin = (e) => {
-    e.preventDefault();
-    fetch("https://journal-policy-tracker.herokuapp.com/users", {
-      method: "POST",
-      mode: "cors",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email: this.state.email,
-        password: this.state.password,
-      }),
-    })
-      .then((response) => response.json())
-      .then((json) => {
-        const accessToken = json.access_token;
-        this.props.onLogin(accessToken);
-      })
-      .catch((error) => {
-        this.props.onLoginError();
-      });
-  };
+	handleLogin = (e) => {
+		e.preventDefault()
+		fetch(
+			"https://journal-policy-tracker.herokuapp.com/users/login",
+			{
+				method: "POST",
+				mode: "cors",
+				headers: {
+					"Content-Type":
+						"application/json",
+				},
+				body: JSON.stringify({
+					email: this.state.email,
+					password: this.state.password,
+				}),
+			}
+		)
+			.then((response) => response.json())
+			.then((res) => {
+				console.log(res)
+				// The response is expected to be in the form:
+				// {
+				//   message: "Login Successful!"
+				//   token: "<Access Token>"
+				// }
 
-  handleEmailChange = (e) => {
-    this.setState({
-      email: e.target.value,
-    });
-  };
+				this.setState({
+					accessToken: res.token,
+				})
+				console.log(this.state.accessToken)
+			})
+			.catch((error) => {
+				console.log(error)
+			})
+	}
 
-  handlePasswordChange = (e) => {
-    this.setState({
-      password: e.target.value,
-    });
-  };
+	handleEmailChange = (e) => {
+		this.setState({
+			email: e.target.value,
+		})
+	}
 
-  render() {
-    return (
-      <Row>
-        <Col md={4}></Col>
-        <Col md={4}>
-          <Form className="login-form" onSubmit={this.handleSubmit}>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter email"
-                name="email"
-                value={this.state.email}
-                onChange={this.handleEmailChange}
-              />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
+	handlePasswordChange = (e) => {
+		this.setState({
+			password: e.target.value,
+		})
+	}
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Password"
-                name="password"
-                value={this.state.password}
-                onChange={this.handlePasswordChange}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Remember me" />
-            </Form.Group>
-            <Button variant="primary" onClick={this.handleLogin}>
-              Login
-            </Button>
-          </Form>
-        </Col>
-        <Col md={4}></Col>
-      </Row>
-    );
-  }
+	render() {
+		return (
+			<Row>
+				<Col md={4}></Col>
+				<Col md={4}>
+					<Form
+						className='login-form'
+						onSubmit={
+							this
+								.handleSubmit
+						}
+					>
+						<Form.Group
+							className='mb-3'
+							controlId='formBasicEmail'
+						>
+							<Form.Label>
+								Email
+								address
+							</Form.Label>
+							<Form.Control
+								type='email'
+								placeholder='Enter email'
+								name='email'
+								value={
+									this
+										.state
+										.email
+								}
+								onChange={
+									this
+										.handleEmailChange
+								}
+							/>
+							<Form.Text className='text-muted'>
+								We'll
+								never
+								share
+								your
+								email
+								with
+								anyone
+								else.
+							</Form.Text>
+						</Form.Group>
+
+						<Form.Group
+							className='mb-3'
+							controlId='formBasicPassword'
+						>
+							<Form.Label>
+								Password
+							</Form.Label>
+							<Form.Control
+								type='password'
+								placeholder='Password'
+								name='password'
+								value={
+									this
+										.state
+										.password
+								}
+								onChange={
+									this
+										.handlePasswordChange
+								}
+							/>
+						</Form.Group>
+						<Form.Group
+							className='mb-3'
+							controlId='formBasicCheckbox'
+						>
+							<Form.Check
+								type='checkbox'
+								label='Remember me'
+							/>
+						</Form.Group>
+						<Button
+							variant='primary'
+							onClick={
+								this
+									.handleLogin
+							}
+						>
+							Login
+						</Button>
+					</Form>
+				</Col>
+				<Col md={4}></Col>
+			</Row>
+		)
+	}
 }
 
-export default Login;
+export default Login
