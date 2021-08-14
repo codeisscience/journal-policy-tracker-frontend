688ad37bd4e0e76e31b60d093cebd39a5c2b8582
Isaac Miti <ikayz@live.com>
2021-08-14 17:55:45 +0200

    Code for handling the submit form added

--- src/components/AddJournal.js
+++ src/components/AddJournal.js
@@ -1,50 +1,97 @@
-import { React, Component } from "react";
+import { React, useState } from "react";
 import { Row, Col, Form, Button } from "react-bootstrap";
 
-class AddJournal extends Component {
-  render() {
-    return (
-      <Row>
-        <Col md={4}></Col>
-        <Col md={4}>
-          <Form className="login-form">
-            <Form.Group className="mb-3" controlId="formBasicTitle">
-              <Form.Label>Title</Form.Label>
-              <Form.Control type="text" placeholder="Journal title" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicUrl">
-              <Form.Label>URL</Form.Label>
-              <Form.Control type="text" placeholder="URL" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicIssn">
-              <Form.Label>ISSN</Form.Label>
-              <Form.Control type="text" placeholder="ISSN" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicRating">
-              <Form.Label>Rating</Form.Label>
-              <Form.Control type="text" placeholder="Rating" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicDate">
-              <Form.Label>Date</Form.Label>
-              <Form.Control type="date" placeholder="Date" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPolicies">
-              <Form.Label>Policies</Form.Label>
-              <Form.Control type="text" placeholder="Policies" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicDomain">
-              <Form.Label>Domain</Form.Label>
-              <Form.Control type="text" placeholder="Domain" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Add Journal
-            </Button>
-          </Form>
-        </Col>
-        <Col md={4}></Col>
-      </Row>
-    );
-  }
-}
+const AddJournal = () => {
+  const [title, setTitle] = useState("");
+  const [url, setUrl] = useState("");
+  const [issn, setIssn] = useState("");
+  const [rating, setRating] = useState("");
+  const [date, setDate] = useState("");
+  const [policies, setPolicies] = useState("");
+  const [domain, setDomain] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const journal = { title, url, issn, rating, date, policies, domain };
+
+    fetch("https://journal-policy-tracker.herokuapp.com/api/journals", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(journal),
+    }).then(() => {
+      console.log("New Journal Added");
+    });
+  };
+
+  return (
+    <Row>
+      <Col md={4}></Col>
+      <Col md={4}>
+        <Form className="login-form" onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicTitle">
+            <Form.Label>Title</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Journal title"
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicUrl">
+            <Form.Label>URL</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="URL"
+              onChange={(e) => setUrl(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicIssn">
+            <Form.Label>ISSN</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="ISSN"
+              onChange={(e) => setIssn(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicRating">
+            <Form.Label>Rating</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Rating"
+              onChange={(e) => setRating(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicDate">
+            <Form.Label>Date</Form.Label>
+            <Form.Control
+              type="date"
+              placeholder="Date"
+              onChange={(e) => setDate(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPolicies">
+            <Form.Label>Policies</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Policies"
+              onChange={(e) => setPolicies(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicDomain">
+            <Form.Label>Domain</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Domain"
+              onChange={(e) => setDomain(e.target.value)}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Add Journal
+          </Button>
+        </Form>
+      </Col>
+      <Col md={4}></Col>
+    </Row>
+  );
+};
 
 export default AddJournal;
