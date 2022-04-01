bb7dd6beedc7987429aab93bd37110bd206cdf8c
predator4hack <chandankum2440@gmail.com>
2022-04-01 15:25:59 +0530

    fixed-header-background-response

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,12 @@ import React from 'react';
 import '../styles/Header.css';
 import { Button, Jumbotron } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
+import img from '../assets/bg.webp';
 
 function Header() {
   return (
     <div className='header'>
+      <img src={img} alt='' className='header-img' />
       <Jumbotron fluid>
         <Button fluid>Read and Sign the Manifesto</Button>
       </Jumbotron>
