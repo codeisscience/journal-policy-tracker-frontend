6e05e74e83b7ea999f98346fad8ef0c257947390
Zoheb Ahmed <zohebcool1542@gmail.com>
2022-05-10 17:39:38 +0530

    bug fixes of contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,90 +4,82 @@ import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import { ImTwitter, ImMail, ImGithub } from 'react-icons/im';
 import { FaGitter } from 'react-icons/fa';
 
-function Contact(){
-  let formIsValid = false
+function Contact() {
+  let formIsValid = false;
 
   const [email, setEmailAttr] = useState({
     value: '',
-    focused: false
-  })
+    focused: false,
+  });
   const [subject, setSubjectAttr] = useState({
     value: '',
-    focused: false
-  })
+    focused: false,
+  });
   const [message, setMessageAttr] = useState({
     value: '',
-    focused: false
-  })
+    focused: false,
+  });
 
+  const setEmailVal = (e) => setEmailAttr((prev) => ({ ...prev, value: e.target.value }));
+  const setSubjectVal = (e) => setSubjectAttr((prev) => ({ ...prev, value: e.target.value }));
+  const setMsgVal = (e) => setMessageAttr((prev) => ({ ...prev, value: e.target.value }));
 
-  const setEmailVal = e => setEmailAttr(prev => ({ ...prev, value: e.target.value }))
-  const setSubjectVal = e => setSubjectAttr(prev => ({ ...prev, value: e.target.value }))
-  const setMsgVal = e => setMessageAttr(prev => ({ ...prev, value: e.target.value }))
+  const setEmailFocus = () => setEmailAttr((prev) => ({ ...prev, focused: true }));
+  const setSubjectFocus = () => setSubjectAttr((prev) => ({ ...prev, focused: true }));
+  const setMsgFocus = () => setMessageAttr((prev) => ({ ...prev, focused: true }));
 
-  const setEmailFocus = () => setEmailAttr(prev => ({ ...prev, focused: true }))
-  const setSubjectFocus = () => setSubjectAttr(prev => ({ ...prev, focused: true }))
-  const setMsgFocus = () => setMessageAttr(prev => ({ ...prev, focused: true }))
+  const emailIsValid = !!email.value;
+  const subjectIsValid = !!subject.value;
+  const messageIsValid = !!message.value;
 
-  const emailIsValid = !!email.value
-  const subjectIsValid = !!subject.value
-  const messageIsValid = !!message.value
+  const emailIsInvalid = email.focused && !emailIsValid;
+  const subjectIsInvalid = subject.focused && !subjectIsValid;
+  const messageIsInvalid = message.focused && !messageIsValid;
 
-  const emailIsInvalid = email.focused && !emailIsValid
-  const subjectIsInvalid = subject.focused && !subjectIsValid
-  const messageIsInvalid = message.focused && !messageIsValid
-
-  formIsValid = emailIsValid && subjectIsValid && messageIsValid
+  formIsValid = emailIsValid && subjectIsValid && messageIsValid;
 
   const formSubmitHandler = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     if (!formIsValid) {
-      console.log("INVALID FORM")
-      return
+      // console.log('INVALID FORM');
+      return;
     }
 
-    console.log("PERFORM NETWORK REQUEST HERE")
-    setEmailAttr({value:'',focused:false})
-    setSubjectAttr({value:'',focused:false})
-    setMessageAttr({value:'',focused:false})
-
-  }
+    // console.log('PERFORM NETWORK REQUEST HERE');
+    setEmailAttr({ value: '', focused: false });
+    setSubjectAttr({ value: '', focused: false });
+    setMessageAttr({ value: '', focused: false });
+  };
 
   return (
     <Container>
-      <h1 className="mt-4">Contact Us</h1>
+      <h1 className='mt-4'>Contact Us</h1>
       <p className='mt-p'>
         Ideas? Comments? Critiques? Want to help out? Here’s how to get in contact:
       </p>
       <Row className='contact-card-one'>
         <Col md={6}>
           <div className='contact-card' style={{ color: '#E74D3C' }}>
-             <a
+            <a
               href='mailto:contact@codeisscience.com'
-              style={{ 'text-decoration': 'none', color: 'inherit' }}
-              >
-            <ImMail size={150} />
-            <p>
-              <a href='mailto:contact@codeisscience.com'>contact@codeisscience.com</a>
-            </p>
+              style={{ textDecoration: 'none', color: 'inherit' }}
+            >
+              <ImMail size={150} />
+              <p style={{ color: '#0D6EFD' }}>contact@codeisscience.com</p>
             </a>
           </div>
         </Col>
         <Col md={6}>
           <div className='contact-card' style={{ color: '#4C4C4D' }}>
             <a
-            href='https://github.com/codeisscience'
-            target='_blank'
-            rel='noreferrer'
-            style={{ 'text-decoration': 'none', color: 'inherit' }}
+              href='https://github.com/codeisscience'
+              target='_blank'
+              rel='noreferrer'
+              style={{ textDecoration: 'none', color: 'inherit' }}
             >
-            <ImGithub size={150} />
-            <p>
-              <a href='https://github.com/codeisscience' target='_blank' rel='noreferrer'>
-                GitHub
-              </a>
-            </p>
+              <ImGithub size={150} />
+              <p style={{ color: '#0D6EFD' }}>GitHub</p>
             </a>
           </div>
         </Col>
@@ -96,17 +88,13 @@ function Contact(){
         <Col md={6}>
           <div className='contact-card' style={{ color: '#3D9DD9' }}>
             <a
-            href='https://twitter.com/codeisscience'
-            target='_blank'
-            rel='noreferrer'
-            style={{ 'text-decoration': 'none', color: 'inherit' }}
+              href='https://twitter.com/codeisscience'
+              target='_blank'
+              rel='noreferrer'
+              style={{ textDecoration: 'none', color: 'inherit' }}
             >
-            <ImTwitter size={150} />
-            <p>
-              <a href='https://twitter.com/codeisscience' target='_blank' rel='noreferrer'>
-                @codeisscience
-              </a>
-            </p>
+              <ImTwitter size={150} />
+              <p style={{ color: '#0D6EFD' }}>@codeisscience</p>
             </a>
           </div>
         </Col>
@@ -114,17 +102,13 @@ function Contact(){
           <div className='contact-card' style={{ color: '#4C4C4D' }}>
             <FaGitter size={150} />
             <a
-            href='https://gitter.im/codeisscience/Lobby'
-            target='_blank'
-            rel='noreferrer'
-            style={{ 'text-decoration': 'none', color: 'inherit' }}
+              href='https://gitter.im/codeisscience/Lobby'
+              target='_blank'
+              rel='noreferrer'
+              style={{ textDecoration: 'none', color: 'inherit' }}
             >
-              <p>
-              <a href='https://gitter.im/codeisscience/Lobby' target='_blank' rel='noreferrer'>
-                Gitter
-              </a>
-            </p>
-             </a>
+              <p style={{ color: '#0D6EFD' }}>Gitter</p>
+            </a>
           </div>
         </Col>
       </Row>
@@ -137,11 +121,13 @@ function Contact(){
                 Email
               </Form.Label>
               <Col sm={10}>
-                <Form.Control type='email'
+                <Form.Control
+                  type='email'
                   value={email.value}
                   onChange={setEmailVal}
                   onBlur={setEmailFocus}
-                  placeholder='someone@example.com' />
+                  placeholder='someone@example.com'
+                />
                 {emailIsInvalid && <p className='invalidInput'>Email cannot be empty.</p>}
               </Col>
             </Form.Group>
@@ -151,11 +137,13 @@ function Contact(){
                 Subject
               </Form.Label>
               <Col sm={10}>
-                <Form.Control type='text'
+                <Form.Control
+                  type='text'
                   value={subject.value}
                   onChange={setSubjectVal}
                   onBlur={setSubjectFocus}
-                  placeholder='Code is Science' />
+                  placeholder='Code is Science'
+                />
                 {subjectIsInvalid && <p className='invalidInput'>Subject cannot be empty.</p>}
               </Col>
             </Form.Group>
@@ -165,12 +153,14 @@ function Contact(){
                 Message
               </Form.Label>
               <Col sm={10}>
-                <Form.Control as="textarea"
+                <Form.Control
+                  as='textarea'
                   value={message.value}
                   rows={3}
                   onChange={setMsgVal}
                   onBlur={setMsgFocus}
-                  placeholder="Enter your message here..." />
+                  placeholder='Enter your message here...'
+                />
                 {messageIsInvalid && <p className='invalidInput'>Message cannot be empty.</p>}
               </Col>
             </Form.Group>
@@ -182,14 +172,16 @@ function Contact(){
 
             <Form.Group as={Row} className='mt-3'>
               <Col sm={{ span: 10, offset: 2 }}>
-                <Button type='submit' disabled={!formIsValid}>Send</Button>
+                <Button type='submit' disabled={!formIsValid}>
+                  Send
+                </Button>
               </Col>
             </Form.Group>
           </Form>
         </Col>
       </Row>
     </Container>
-  )
-};
+  );
+}
 
-export default Contact
+export default Contact;
