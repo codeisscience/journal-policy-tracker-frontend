71905e525eed66bddb9f604eaefa4118d4ba6988
Padmaja Bhol <75530516+padmajabhol@users.noreply.github.com>
2022-06-20 12:25:05 +0530

    Move content to config and images to cloudinary

---
+++ config/content/navbar.js
@@ -0,0 +1,34 @@
+export default {
+    title: 'Navbar',
+    logo: {
+        src: 'https://res.cloudinary.com/dh6j2zoog/image/upload/v1655702059/CiS/logo_ch4fzv.png',
+        alt: 'Code Is Science',
+    },
+    navItems: [
+        {
+            id: 'home',
+            name: 'Home',
+            link: '/home',
+        },
+        {
+            id: 'journals',
+            name: 'Journals',
+            link: '/journals',
+        },
+        {
+            id: 'manifesto',
+            name: 'Manifesto',
+            link: '/manifesto',
+        },
+        {
+            id: 'contact',
+            name: 'Contact',
+            link: '/contact',
+        },
+        {
+            id: 'login',
+            name: 'Login',
+            link: '/login',
+        },
+    ],
+};
