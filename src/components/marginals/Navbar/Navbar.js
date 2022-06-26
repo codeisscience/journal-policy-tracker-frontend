ea9a16020f388e8e73b13f261fbcab6f3d98c328
Padmaja Bhol <75530516+padmajabhol@users.noreply.github.com>
2022-06-27 03:28:06 +0530

    Redo navbar

---
+++ src/components/marginals/Navbar/Navbar.js
@@ -0,0 +1,32 @@
+/* eslint-disable react/self-closing-comp */
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Nav, Logo, Hamburger, Menu, MenuLink, Button, Items } from './styles';
+import { navbar } from  '../../../config/content';
+
+function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <Nav>
+            <Logo src={navbar.logo.src}/>
+            <Button onClick={() => setIsOpen(!isOpen)}>
+            {isOpen ? 
+                 <FontAwesomeIcon icon = {navbar.icon2}/> :
+                 <FontAwesomeIcon icon={ navbar.icon } /> } 
+            </Button>
+            <Menu isOpen={isOpen}>
+                <Link to='/'><Items>Home</Items></Link>
+                <Link to='/manifesto'><Items>Manifesto</Items></Link>
+                <Link to='/journal'><Items>Journal</Items></Link>
+                <Link to='/contact'><Items>Contact</Items></Link>
+                <Link to='/login'><Items>Login</Items></Link>
+                <Link to='/signup'><Items>SignUp</Items></Link>
+            </Menu>
+        </Nav>
+    );
+};
+
+export default Navbar
---
+++ src/components/marginals/Navbar/styles.js
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  display: none;
+  border: none;
+  font-size: 20px;
+  flex-direction: column;
+  cursor: pointer;
+  @media (max-width: 768px) {
+    display: flex;
+  }
+`;
+
+export const MenuLink = styled.a`
+  padding: 1rem 2rem;
+  cursor: pointer;
+  text-align: center;
+  text-decoration: none;
+  color: #67bc98;
+  transition: all 0.3s ease-in;
+  font-size: 0.9rem;
+  &:hover {
+    color: #7b7fda;
+  }
+`;
+
+export const Nav = styled.div`
+  padding: 0 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+  background: white;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+`;
+
+export const Logo = styled.img`
+  width: 9rem;
+`;
+
+export const Menu = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+  @media (max-width: 768px) {
+    overflow: hidden;
+    flex-direction: column;
+    max-height: ${({ isOpen }) => (isOpen ? "300px" : "0")};
+    transition: max-height 0.3s ease-in;
+    width: 100%;
+  }
+`;
+
+export const Hamburger = styled.div`
+  display: none;
+  flex-direction: column;
+  cursor: pointer;
+  span {
+    height: 2px;
+    width: 25px;
+    background: #7b7fda;
+    margin-bottom: 4px;
+    border-radius: 5px;
+  }
+  @media (max-width: 768px) {
+    display: flex;
+  }
+`;
+
+export const Items = styled.li`
+  text-decoration: none;
+  list-style: none;
+  color: black;
+  padding: 0 10px;
+  margin: 0 10px;
+  cursor: pointer;
+  text-transform: capitalize;
+`;
